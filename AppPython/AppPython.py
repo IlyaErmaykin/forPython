b7c5e9ff781086ab098635a2aaadbcfcbd693855
IErmaykin <IErmaykin@ibs.ru>
2019-10-08 14:31:33 +0400

    #4 - ForPython;  - Калькулятор;

--- AppPython/AppPython.py
+++ AppPython/AppPython.py
@@ -1,5 +1,20 @@
-X = int(input());
-H = int(input());
-M = int(input());
-#TODO
-#print(X//60 + H//, (X % 60 + M), sep='\n');
+a = float(input());
+b = float(input());
+op = str(input());
+if(op == "+"):
+    res = ((a) + (b));
+elif (op == "-"):
+    res = ((a) - (b));
+elif (op == "*"):
+    res = ((a) * (b));
+elif (op == "/") and (b != 0.0):
+    res = ((a) / (b));
+elif (op == "mod") and (b != 0.0):
+    res = ((a) % (b));
+elif (op == "pow"):
+    res = ((a) ** (b));
+elif (op == "div") and (b != 0.0):
+    res = ((a) // (b));
+else:
+    res = "Деление на 0!";
+print(res);
