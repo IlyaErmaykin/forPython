9e9ee59cbef36854da95f107e4bccaf968af8ca5
IErmaykin <IErmaykin@ibs.ru>
2019-10-16 18:57:27 +0400

    #10 - ForPython;  - Задача составления таблицы умножения;

--- AppPython/AppPython.py
+++ AppPython/AppPython.py
@@ -1,7 +1,22 @@
-a = str(input());
+a = int(input());
+b = int(input());
+c = int(input());
+d = int(input());
+print(" ", end=" ");
+for x in range (c,d + 1):
+    print(x, end=" ");
+    x += 1;
+    continue;
+print();
 
-if (int(a[0]) + int(a[1]) + int(a[2]) == int(a[3]) + int(a[4]) + int(a[5])):
-    print("Счастливый");
-else:
-    print("Обычный");
+for i in range (a, b + 1):
+    for j in range (c - 1, d + 1):
+        if (j == c - 1):
+            print(i, end=" ");
+            continue;
+        print(i * j, end=" ");
+        j += 1;
+    print();
+    i += 1;
+        
 
