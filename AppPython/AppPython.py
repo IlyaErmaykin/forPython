4ddf93d7ee491c7f1cf791b94a4bc05389b96f19
IErmaykin <IErmaykin@ibs.ru>
2019-10-16 13:01:13 +0400

    #8 - ForPython;  - Задача подсчета программистов в комнате;

--- AppPython/AppPython.py
+++ AppPython/AppPython.py
@@ -1,19 +1,28 @@
-f = str(input());
-a = 0;
-b = 0;
-c = 0;
-pi = 3.14;
-p = 0;
-S = 0.0;
+a = int(input());
 
-if (f == "треугольник"):
-    a = int(input());
-    b = int(input());
-    c = int(input());
-    p = (a + b + c) / 2;
-    S = ((p * (p - a) * (p - b) * (p - c)) ** 0.5)
-elif (f == "прямоугольник"):
-    S = (int(input()) * int(input()));
+strA = str(a);
+lenStrA = int(len(strA) - 1);
+
+if (lenStrA == 0):
+    if (a < 1 or 5 <= a <= 9):
+        print(strA + " программистов");
+    elif (2 <= a <=4):
+        print(strA + " программиста");
+    else:
+        print(strA + " программист");
+elif (lenStrA == 1 ):
+    if (a % 10 == 1 and (a != 11)):
+        print(strA + " программист");
+    elif (a != 12 and a != 13 and a != 14 and (a % 10 == 2 or a % 10 == 3 or a % 10 == 4)):
+        print(strA + " программиста");
+    else:
+        print(strA + " программистов");
+elif (lenStrA == 2 ):
+     if (a != 111 and a != 112 and a != 113 and (a % 10 == 1)):
+        print(strA + " программист");
+     elif (a % 100 != 11 and a % 100 != 12 and a % 100 != 13 and a % 100 != 14 and (a % 10 == 2 or a % 10 == 3 or a % 10 == 4)):
+        print(strA + " программиста");
+     else:
+        print(strA + " программистов");
 else:
-    S = (pi * (int(input()) ** 2));
-print(float(S));
+    print(strA + " программистов");
