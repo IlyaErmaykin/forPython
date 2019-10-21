0e3312309a8b9f56c6a8808b17f962a8bc8fe6f2
IErmaykin <IErmaykin@ibs.ru>
2019-10-21 10:39:54 +0400

    #12 - ForPython;  - Задача подсчета последовательности символов;  - Начальная реализация;  - Не доделано;

--- AppPython/AppPython.py
+++ AppPython/AppPython.py
@@ -1,12 +1,37 @@
-s = str(input());
-sL = s.lower();
-strNum = 0;
-res = 0;
-n = 0;
-for i in sL:
-    strNum = strNum + 1;
+# ДОДЕЛАТЬ
+#string = str('aaaabbcaa'); #str(input());
+#result = ""
+#while len(string) != 0 :
 
-n = sL.count("c") + sL.count("g");
-res = float((n / strNum) * 100);
-print(res);     
+#    count = len(string);
+#    cnt = 1;
+#    symbol = 0;
+#    symbol = string[0]
+#    for key in range(count):
+#        print(string[key]);
+#        if key <= count - 2 :
+#            if string[key] == string[key + 1] :
+#                cnt += 1
+#            else : break;
+#    #for index, key in string:
+#     #   del string();    
+#    #[0, str(cnt - 1)];
+#    result += symbol + str(cnt)
 
+
+
+
+
+
+#while i <= d:
+#    if(s[i + 1] !=  and s[i + 1] == s[i]):
+#        print(s[i - 1] + str(i - 1));        
+#    i += 1;
+    #while (d <= 1):
+    #    print(s[c] + str(i), end="");
+    #    c = 0;            
+    #elif (s[i] == s[i + 1]):
+    #    c += 1;
+    #else:
+    #    continue;
+#print(s[9]);
