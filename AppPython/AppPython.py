55b46eec9a76468c70113664982d32cf13fbe4c1
IErmaykin <IErmaykin@ibs.ru>
2019-10-21 15:53:44 +0400

    #13 - ForPython;  - Задача подсчета последовательности символов;  - Доделано;

--- AppPython/AppPython.py
+++ AppPython/AppPython.py
@@ -1,37 +1,18 @@
-# ДОДЕЛАТЬ
-#string = str('aaaabbcaa'); #str(input());
-#result = ""
-#while len(string) != 0 :
-
-#    count = len(string);
-#    cnt = 1;
-#    symbol = 0;
-#    symbol = string[0]
-#    for key in range(count):
-#        print(string[key]);
-#        if key <= count - 2 :
-#            if string[key] == string[key + 1] :
-#                cnt += 1
-#            else : break;
-#    #for index, key in string:
-#     #   del string();    
-#    #[0, str(cnt - 1)];
-#    result += symbol + str(cnt)
-
-
-
-
-
-
-#while i <= d:
-#    if(s[i + 1] !=  and s[i + 1] == s[i]):
-#        print(s[i - 1] + str(i - 1));        
-#    i += 1;
-    #while (d <= 1):
-    #    print(s[c] + str(i), end="");
-    #    c = 0;            
-    #elif (s[i] == s[i + 1]):
-    #    c += 1;
-    #else:
-    #    continue;
-#print(s[9]);
+s = str(input());
+d = len(s);
+c = 1;
+r = "";
+for i in range(d):
+        if (d - i == 1):
+            print(s[i] + str(c), end="");
+            continue;
+        if(s[i] != s[i + 1]):
+            print(s[i] + str(c), end="");
+            c = 1;
+        elif(s[i] == s[i + 1]):
+            r = s[i] + str(c);
+            c += 1;
+        else:
+            print(r);
+            r = "";
+            c = 1;
