60866f8d5e5bdb4306046e8079ecf21b5f4205a6
IErmaykin <IErmaykin@ibs.ru>
2019-10-21 17:08:08 +0400

    #14 - ForPython;  - Задача подсчета суммы списка;

--- AppPython/AppPython.py
+++ AppPython/AppPython.py
@@ -1,18 +1,6 @@
-s = str(input());
-d = len(s);
-c = 1;
-r = "";
-for i in range(d):
-        if (d - i == 1):
-            print(s[i] + str(c), end="");
-            continue;
-        if(s[i] != s[i + 1]):
-            print(s[i] + str(c), end="");
-            c = 1;
-        elif(s[i] == s[i + 1]):
-            r = s[i] + str(c);
-            c += 1;
-        else:
-            print(r);
-            r = "";
-            c = 1;
+a = input().split();
+s = 0;
+for i in range(len(a)):
+    s = s + int(a[i]);
+print(s);
+    
