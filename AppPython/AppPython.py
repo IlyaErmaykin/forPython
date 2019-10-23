136ce8fc05792f2cc5c285e7a911d42cbd967da4
IErmaykin <IErmaykin@ibs.ru>
2019-10-23 12:55:19 +0400

    #16 - ForPython;  - Задача поиска повторяющихся символов;  - Не совсем корректное решение, пожелание переделать;

--- AppPython/AppPython.py
+++ AppPython/AppPython.py
@@ -1,3 +1,13 @@
+#Переделать
 a = [int(i) for i in input().split()];
-print(int(sum(a)));
-    
+s = 0;
+a.sort();
+a.append('0');
+n = a[0];
+for i in a:
+    if n != i:    
+        if s >= 2:
+            print(str(n), end=" ");
+        s = 0;
+        n = i;
+    s += 1;
