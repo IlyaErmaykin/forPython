91a132a3b2beba307cc01215d3761bc542b3b4dc
IErmaykin <IErmaykin@ibs.ru>
2019-10-24 15:12:13 +0400

    #17 - ForPython;  - Задача поиска повторяющихся символов в строке;

--- AppPython/AppPython.py
+++ AppPython/AppPython.py
@@ -1,13 +1,12 @@
-#Переделать
 a = [int(i) for i in input().split()];
-s = 0;
-a.sort();
-a.append('0');
-n = a[0];
-for i in a:
-    if n != i:    
-        if s >= 2:
-            print(str(n), end=" ");
-        s = 0;
-        n = i;
-    s += 1;
+n = int(input());
+f = False;
+
+for i in range(len(a)):
+    if(a[i] == n):
+        print(i, end=" ");
+        f = True;
+    else:
+        continue;
+if(not f):
+    print("Отсутствует");
