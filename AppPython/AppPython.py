3db75de3e1be885c82570f6751f1c727d0d90c7e
IErmaykin <IErmaykin@ibs.ru>
2020-01-14 11:32:42 +0400

    #35 - ForPython  - Задача  поиска заданного файла в интеренет каталоге с помощью библиотеки "Requests";  - Реализована замена url-пути;  - Добавлена проверка по содержаню файла;

--- AppPython/AppPython.py
+++ AppPython/AppPython.py
@@ -1,8 +1,18 @@
 import requests;
 
-url = 'https://stepic.org/media/attachments/course67/3.6.2/854.txt';
-r = requests.get(url);
+f = True;
+url = 'https://stepic.org/media/attachments/course67/3.6.3/';
+urlFile = '699991.txt';
+r = requests.get(url + urlFile);
+print(r.text);
 
-print(len(r.text.splitlines(True)));
-#for line in r:
+while(f):
+    if(r.text[:2] != "We"):
+        r = requests.get(url + r.text);
+        print(r.text);
+        continue;
+    else:
+        print(r.text);
+        f = False;
+        break;
 
