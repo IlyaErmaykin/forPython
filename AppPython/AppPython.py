e6412ff74b427f89abfaf4ffff4a87b2e2439332
IErmaykin <IErmaykin@ibs.ru>
2020-01-14 10:33:13 +0400

    #31 - ForPython  - Задача подсчета строк файла, скачанного из интеренета с помощью примере библиотеки "Requests";

--- AppPython/AppPython.py
+++ AppPython/AppPython.py
@@ -1,16 +1,8 @@
 import requests;
 
-#r = requests.get('http://example.com');        # Get запрос
-#print(r.text);                                 # Вывод ответа от сервера
+url = 'https://stepic.org/media/attachments/course67/3.6.2/854.txt';
+r = requests.get(url);
 
-#url = 'http://example.com';
-#par = {'key1': 'value1', 'key2': 'value2'};
-#r = requests.get(url, par);                    # Передача параметров в запрос 
-#print(r.url);                                  # Вывод url-адреса с учетом параметров Get запроса
+print(len(r.text.splitlines(True)));
+#for line in r:
 
-url = 'http://httpbin.org/cookies';
-cookies = {'cookies_age': 'working'};
-r = requests.get(url, cookies);                 # Отправка сформированных cookies на сервер
-print(r.text);
-
-print(r.cookies['example_cookie_name']);        # Использование cookies, полученных от сервера
