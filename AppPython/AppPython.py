7c8726174e6ddee3adf35544b4a721787dcc92b8
IErmaykin <IErmaykin@ibs.ru>
2020-01-14 12:52:36 +0400

    #36 - ForPython  - Задача  поиска координатной точки по посдедовательности введенных команд;

--- AppPython/AppPython.py
+++ AppPython/AppPython.py
@@ -1,18 +1,16 @@
-import requests;
+n = int(input());
+x = 0;
+y = 0;
 
-f = True;
-url = 'https://stepic.org/media/attachments/course67/3.6.3/';
-urlFile = '699991.txt';
-r = requests.get(url + urlFile);
-print(r.text);
-
-while(f):
-    if(r.text[:2] != "We"):
-        r = requests.get(url + r.text);
-        print(r.text);
-        continue;
+for play in range(n):
+    a = input().split();
+    if (a[0].lower() == "север"):
+        y = y + int(a[1]);
+    elif (a[0].lower() == "восток"):
+        x = x + int(a[1]);
+    elif (a[0].lower() == "юг"):
+        y = y - int(a[1]);
     else:
-        print(r.text);
-        f = False;
-        break;
+        x = x - int(a[1]);
 
+print(x, y);
